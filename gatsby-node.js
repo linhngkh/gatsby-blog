29eba5f8ead4c029324b5bb044c4002b461662c0
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-29 20:54:27 +0300

    createpage api setting

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,14 +19,17 @@ exports.createPages = async ({ actions, graphql }) => {
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
-        component: 
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug,
+        },
       })
     })
   })
 }
 
 const { createFilePath } = require(`gatsby-source-filesystem`)
-
+const path = require("path")
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
