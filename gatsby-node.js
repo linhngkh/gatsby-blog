689560153da3362df766dcd5ca980f5df1207203
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-29 20:22:39 +0300

    create slug for each blog dynamically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,4 @@
 /**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
-
-/**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
 exports.createPages = async ({ actions }) => {
@@ -15,4 +9,21 @@ exports.createPages = async ({ actions }) => {
     context: {},
     defer: true,
   })
+}
+
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({
+      node,
+      getNode,
+    })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
 }
